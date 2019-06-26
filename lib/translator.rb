c0b7a5969032cb51b2aec2c9751f8dd395e05773
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-26 00:03:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,9 @@
-# require modules here
 
-def load_library
-  # code goes here
+
+def load_library(get_meaning, get_emoticon)
+"Hello #{get_meaning} and my emotion is #{get_emoticon}."
 end
+load_library("(ΘεΘ;)", ":O")
 
 def get_japanese_emoticon
   # code goes here
