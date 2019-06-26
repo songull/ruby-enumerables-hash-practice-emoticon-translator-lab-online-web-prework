117cb0c127bd4cf9bae8b9c226272e53d72d006c
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-26 00:04:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,41 @@
 
 
-def load_library(get_meaning, get_emoticon)
-"Hello #{get_meaning} and my emotion is #{get_emoticon}."
+def load_library
+  ---
+  receipt:     Oz-Ware Purchase Invoice
+  date:        2012-08-06
+  customer:
+      first_name:   Dorothy
+      family_name:  Gale
+  
+  items:
+      - part_no:   A4786
+        descrip:   Water Bucket (Filled)
+        price:     1.47
+        quantity:  4
+  
+      - part_no:   E1628
+        descrip:   High Heeled "Ruby" Slippers
+        size:      8
+        price:     133.7
+        quantity:  1
+  
+  bill-to:  &id001
+      street: |
+              123 Tornado Alley
+              Suite 16
+      city:   East Centerville
+      state:  KS
+  
+  ship-to:  *id001
+  
+  specialDelivery:  >
+      Follow the Yellow Brick
+      Road to the Emerald City.
+      Pay no attention to the
+      man behind the curtain.
+  ...  
 end
-load_library("(ΘεΘ;)", ":O")
 
 def get_japanese_emoticon
   # code goes here
